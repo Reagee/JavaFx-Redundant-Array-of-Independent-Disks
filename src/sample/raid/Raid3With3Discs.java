92761bd1982bc8899cca3c02035579e5ad38b6a2
damiand96 <damiand96@o2.pl>
2018-06-18 20:24:52 +0200

    DataSplitter can split data for RAID3 with 3 or 4 discs (2 or 3 discs + 1 parity disc).

--- src/sample/raid/Raid3With3Discs.java
+++ src/sample/raid/Raid3With3Discs.java
@@ -1,26 +1,26 @@
 package sample.raid;
 
 public class Raid3With3Discs {
-    private StringBuffer disc1;
-    private StringBuffer disc2;
-    private StringBuffer discParity;
+    private String disc1;
+    private String disc2;
+    private String discParity;
 
-    public Raid3With3Discs(StringBuffer disc1, StringBuffer disc2, StringBuffer discParity) {
+    public Raid3With3Discs(String disc1, String disc2, String discParity) {
 
         this.disc1 = disc1;
         this.disc2 = disc2;
         this.discParity = discParity;
     }
 
-    public StringBuffer getDisc1() {
+    public String getDisc1() {
         return disc1;
     }
 
-    public StringBuffer getDisc2() {
+    public String getDisc2() {
         return disc2;
     }
 
-    public StringBuffer getDiscParity() {
+    public String getDiscParity() {
         return discParity;
     }
 }
--- test/sample/raid/DataSplitterTest.java
+++ test/sample/raid/DataSplitterTest.java
@@ -13,14 +13,29 @@ class DataSplitterTest {
         String input = "100000001100000011100000111100001111100011111100";
 
         //WHEN
-        dataSplitter.splitInto3Discs(input);
+        Raid3With3Discs raid3With3Discs = dataSplitter.splitInto3Discs(input);
 
         //THEN
-        Assertions.assertArrayEquals();
+        Assertions.assertEquals("100000001110000011111000",raid3With3Discs.getDisc1());
+        Assertions.assertEquals("110000001111000011111100",raid3With3Discs.getDisc2());
+        Assertions.assertEquals("010000000001000000000100",raid3With3Discs.getDiscParity());
     }
 
     @Test
     void splitInto4Discs() {
+
+        //GIVEN
+        DataSplitter dataSplitter = new DataSplitter();
+        String input = "100000001100000011100000111100001111100011111100";
+
+        //WHEN
+        Raid3With4Discs raid3With4Discs = dataSplitter.splitInto4Discs(input);
+
+        //THEN
+        Assertions.assertEquals("1000000011110000",raid3With4Discs.getDisc1());
+        Assertions.assertEquals("1100000011111000",raid3With4Discs.getDisc2());
+        Assertions.assertEquals("1110000011111100",raid3With4Discs.getDisc3());
+        Assertions.assertEquals("1010000011110100",raid3With4Discs.getDiscParity());
     }
 
 }
